657828d93b2f62a27738fa0b24911ce3725e34f8
fe3524 <fe212045@grace.ocn.ne.jp>
2021-10-17 16:28:59 +0900

    itemsテーブルの不要カラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_17_072432) do
+ActiveRecord::Schema.define(version: 2021_10_17_072748) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 2021_10_17_072432) do
     t.integer "day_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
-    t.integer "status"
     t.integer "status_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
