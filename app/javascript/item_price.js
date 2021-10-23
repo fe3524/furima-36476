f961078813056babe38a2e6078bf346192144b4e
fe3524 <fe212045@grace.ocn.ne.jp>
2021-10-23 11:16:35 +0900

    バリデーションの修正／エラーメッセージ表示

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,14 +1,12 @@
 window.addEventListener('load', () => {
-  console.log("OK");
-  const priceInput = document.getElementById("item-price");
-  console.log(priceInput);
+const priceInput = document.getElementById("item-price");
 
   priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  console.log(inputValue);
-
   const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+  const profit = document.getElementById("profit");
+  profit.innerHTML = Math.floor(inputValue - (inputValue * 0.1));
+  })
 
-})
 });
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   validates :fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true
+  validates :image, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
 
