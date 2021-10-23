51bb6cc7f5121287ccb18404ed911940a24ea9eb
fe3524 <fe212045@grace.ocn.ne.jp>
2021-10-23 10:08:31 +0900

    販売手数料の計算と表示機能実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,10 +2,13 @@ window.addEventListener('load', () => {
   console.log("OK");
   const priceInput = document.getElementById("item-price");
   console.log(priceInput);
+
   priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
   console.log(inputValue);
-  const addTaxDom = document.getElementById("price-content");
-  addTaxDom.innerHTML = "入力した金額をもとに販売手数料を計算する処理"
+
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+
 })
 });
