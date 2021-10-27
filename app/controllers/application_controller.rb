b854eb52fb8a6d18d013a25282b338b6c14892e3
fe3524 <fe212045@grace.ocn.ne.jp>
2021-10-27 17:53:28 +0900

    ログアウト時挙動の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth # BASIC認証
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   private
 
