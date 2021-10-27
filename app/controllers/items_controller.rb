6046116a35dc5da1114dbbbc5a879a965a02af84
fe3524 <fe212045@grace.ocn.ne.jp>
2021-10-27 18:03:16 +0900

    ログアウト時挙動の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @items = Item.history('created_at DESC')
+    @items = Item.order('created_at DESC')
   end
 
   def new
