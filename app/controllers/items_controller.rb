f0165ba589a7b36cd587cc2285e86b008469b8cc
fe3524 <fe212045@grace.ocn.ne.jp>
2021-10-27 21:26:03 +0900

    ログアウト状態の挙動実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create, :edit, :show]
 
   def index
     @items = Item.order('created_at DESC')
