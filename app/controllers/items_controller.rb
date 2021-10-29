7f613246ebf982b2529a1156fd35362959ef0e3c
fe3524 <fe212045@grace.ocn.ne.jp>
2021-10-29 14:25:00 +0900

    itemコントローラーのリファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :redirect_to_root, only: [:edit, :update]
+  before_action :item_find, only: [:edit, :show, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,16 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def show
-    @items = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
-
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -56,5 +53,9 @@ class ItemsController < ApplicationController
   def redirect_to_root
     @item = Item.find(params[:id])
     redirect_to root_path unless @item.user_id == current_user.id
+  end
+
+  def item_find
+    @item = Item.find(params[:id])
   end
 end
