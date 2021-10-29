19dbf21f1c564abb0ff1d4ae8e448bc18a7790bd
fe3524 <fe212045@grace.ocn.ne.jp>
2021-10-29 13:25:36 +0900

    編集成功後_詳細画面遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,6 +31,12 @@ class ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     item.update(item_params)
+
+    if item.update(item_params)
+      redirect_to item_path(item.id)
+    else
+      render :edit
+    end
   end
 
   private
