dc5e9d3f257c7b0a9baef4a94db45558a2ed9508
fe3524 <fe212045@grace.ocn.ne.jp>
2021-11-01 09:30:36 +0900

    クレジット決済機能：Form情報の取得

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,18 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_b993505000919d0533fd3164"); // PAY.jpテスト公開鍵
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("history[number]"),
+      cvc: formData.get("history[cvc]"),
+      exp_month: formData.get("history[exp_month]"),
+      exp_year: `20${formData.get("history[exp_year]")}`,
+    };
   });
 };
 
