61d42a65b5847637d884347f8bc2e42eedd9a21f
fe3524 <fe212045@grace.ocn.ne.jp>
2021-11-01 16:31:42 +0900

    コードレビュー後修正①

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe Form, type: :model do
   describe '商品購入' do
     before do
-      @user = FactoryBot.create(:user)
-      @item = FactoryBot.create(:item)
-      @form = FactoryBot.build(:form, user_id: @user.id, item_id: @item.id)
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      form = FactoryBot.build(:form, user_id: @user.id, item_id: @item.id)
       sleep 0.1
     end
 
