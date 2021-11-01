b9a4bbbbd2a91bbdda813a77ea5b798a9aa2f1ce
fe3524 <fe212045@grace.ocn.ne.jp>
2021-11-01 16:36:13 +0900

    コードレビュー後修正②

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Form, type: :model do
     before do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
-      form = FactoryBot.build(:form, user_id: @user.id, item_id: @item.id)
+      @form = FactoryBot.build(:form, user_id: @user.id, item_id: @item.id)
       sleep 0.1
     end
 
